2bc699107b55d0304ee048a0e745fffe319da854
ana-lepage <alepage_2030im@alumni.uma.edu.ve>
2026-07-01 01:19:01 -0400

    prueba

--- prueba proyecto/backend/__init__.py
+++
@@ -1,4 +0,0 @@
-from .modelos import Usuario, Carro, Puja, hash_password
-from .sistema import AdministradorCompraVenta
-
-__all__ = ["Usuario", "Carro", "Puja", "hash_password", "AdministradorCompraVenta"]
